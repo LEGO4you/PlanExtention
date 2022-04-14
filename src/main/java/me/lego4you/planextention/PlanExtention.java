45cde9f932e18d8e47cecab0d161dd596b210cde
LEGO4you <jelle.willem.meinesz@gmail.com>
2022-04-14 21:28:39 +0200

    update

--- src/main/java/me/lego4you/planextention/PlanExtention.java
+++ src/main/java/me/lego4you/planextention/PlanExtention.java
@@ -1,12 +1,25 @@
 package me.lego4you.planextention;
 
+import me.lego4you.planextention.extention.PlanHook;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 public final class PlanExtention extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        try {
+            new PlanHook();
+        } catch (NoClassDefFoundError planIsNotInstalled) {
+            PluginLogger logger = (PluginLogger) Bukkit.getLogger();
+            logger.log(new LogRecord(Level.ALL, "Plan Player Analytics is not installed! please install it to propperly enable this plugin"));
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
 
     }
 
